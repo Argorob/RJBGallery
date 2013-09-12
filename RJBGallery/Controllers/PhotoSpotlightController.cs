72fe68f9a2b53d6f3359cf3efaf0e4222eb4ee5d
Robert Burr <RobertBurr@molabyte.com>
2013-09-12 07:34:32 -0500

    Initial commit

---
+++ RJBGallery/Controllers/PhotoSpotlightController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RJBGallery.Models;
+
+namespace RJBGallery.Controllers
+{
+    public class PhotoSpotlightController : Controller
+    {
+        //
+        // GET: /PhotoSpotlight/
+
+        public ActionResult Index()
+        {
+            Photo ph = new Photo();
+              ph.photoName = "Robert";
+            
+            return View(ph);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Photo photoObj)
+        {
+            
+
+            return View(photoObj);
+        }
+
+    }
+}
---
+++ RJBGallery/Models/Photo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RJBGallery.Models
+{
+    public class Photo
+    {
+        public string photoName
+        {
+            get;
+            set;
+        }
+
+        public Photo(string temp_photoName)
+        {
+            photoName = temp_photoName;
+        }
+
+        public Photo()
+        {
+        }
+
+    }
+}
